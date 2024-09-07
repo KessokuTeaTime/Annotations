abfc805ae438882795a4f62b5a6da4f61a4f549b
RawDiamondMC <RawDiamondMC@outlook.com>
2024-09-07 20:41:20 +0800

    wlg value

--- src/main/java/band/kessoku/annotations/Environment.java
+++ src/main/java/band/kessoku/annotations/Environment.java
@@ -6,6 +6,8 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PACKAGE})
 @Documented
 public @interface Environment {
+    Type value();
+
     enum Type {
         CLIENT,
         SERVER
